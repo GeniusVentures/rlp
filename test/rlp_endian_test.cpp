1e7fb8298ee6146a425485963043e885c9ccda70
stefan <stefan.dyulgerov@gmail.com>
2025-09-09 23:10:28 +0300

    added skeleton for endian tests

---
+++ test/rlp_endian_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include <endian.hpp>
+
+using namespace rlp;
+
+TEST(Endian, Encodeuint8_t) {
+  // TODO: Add actual test logic
+}
+
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
