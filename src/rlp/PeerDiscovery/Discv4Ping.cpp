af80ff5798e649bcaf7528c9061f9fc042d555d0
stefan <stefan.dyulgerov@gmail.com>
2025-11-02 17:33:11 +0200

    Refactor: Adapt Discv4Ping::RlpPayload to EncodingResult

--- src/rlp/PeerDiscovery/Discv4Ping.cpp
+++ src/rlp/PeerDiscovery/Discv4Ping.cpp
@@ -7,15 +7,34 @@ namespace discv4 {
 std::vector<uint8_t> Discv4Ping::RlpPayload()
 {
     rlp::RlpEncoder encoder;
-    encoder.BeginList();
-    encoder.add( Version() );
-    encoder.AddRaw( fromEp.encode() );
-    encoder.AddRaw( toEp.encode() );
-    auto data = fromEp.encode();
+    if (auto res = encoder.BeginList(); !res) {
+        return std::vector<uint8_t>(); // Return empty on error
+    }
+    if (auto res = encoder.add( Version() ); !res) {
+        return std::vector<uint8_t>();
+    }
+    auto from_encoded = fromEp.encode();
+    if (!from_encoded) {
+        return std::vector<uint8_t>();
+    }
+    if (auto res = encoder.AddRaw( from_encoded.value() ); !res) {
+        return std::vector<uint8_t>();
+    }
+    auto to_encoded = toEp.encode();
+    if (!to_encoded) {
+        return std::vector<uint8_t>();
+    }
+    if (auto res = encoder.AddRaw( to_encoded.value() ); !res) {
+        return std::vector<uint8_t>();
+    }
     uint32_t now = static_cast<std::uint32_t>( std::time( nullptr ) );
     uint32_t expire_in_1_minute = now + 60;
-    encoder.add( expire_in_1_minute );
-    encoder.EndList();
+    if (auto res = encoder.add( expire_in_1_minute ); !res) {
+        return std::vector<uint8_t>();
+    }
+    if (auto res = encoder.EndList(); !res) {
+        return std::vector<uint8_t>();
+    }
 
     auto bytes_result = encoder.MoveBytes();
     if (!bytes_result) {
